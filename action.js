e2cb5eb17a41e0a07ca9ad2a025883f5c1da29ea
JamesCBlackwell <31408542+JamesCBlackwell@users.noreply.github.com>
2017-11-01 18:49:50 -0400

    Add files via upload

--- action.js
+++ action.js
@@ -1,3 +1,16 @@
+function checkForBracket() {
+    var regEx = new RegExp('<') ;
+
+    if(regEx.test(document.getElementById('name').value)) {
+        console.log('contains illegal character') ;
+        //if illegal character it turns red
+        document.getElementById('name').style.backgroundColor = 'red' ;
+        //if not illegal character it does not change color
+    } else {
+        document.getElementById('name').style.backgroundColor = '' ;
+    }
+}
+
 function checkEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
@@ -6,10 +19,19 @@ function checkEmail(email) {
 
 function checkAge() {
     var regEx = new RegExp('\w');
-    
-    if() {
-       
-    } else {
-       
+
+    if(regEx.test(document.getElementById('age').value)){
+        document.getElementById('age')style.backgroundColor='red' ;
+    } else{
+        document.getElementsById('age').style.backgroundColor='' ;
+    }
+}
+
+function checkName() {
+    var regEx = new RegExp('<') ;
+    if(regEx.test(document.getElementById('name').value)){
+        document.getElementById('name')style.backgroundColor='red' ;
+    } else{
+        document.getElementsById('name').style.backgroundColor='' ;
     }
 }
